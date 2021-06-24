824d546e7d759431f111f62a1a05386915e57992
vishal804 <vishal804>
2021-06-24 14:07:15 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { emojiDictionary } from "./emojis";
+import "./styles.css";
 
-function App() {
+var emoji = Object.keys(emojiDictionary);
+
+export default function App() {
+  const [meaning, setMeaning] = useState("");
+
+  function emojiInput(event) {
+    var userInput = event.target.value;
+    setMeaning(emojiDictionary[userInput]);
+  }
+
+  function clickHandler(item) {
+    var meaning = emojiDictionary[item];
+    setMeaning(meaning);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+  <div className="App">
+      <header id="heading">
+      <h1>Emojipedia</h1>
+      <h3 id="sub-heading">Confuse with the meaning of Emoji 
+      <br></br>
+      Well check it out here! Just type an Emoji in the search bar or Select from below
+      </h3>
       </header>
+      
+    <section id="content">
+    <div id="serachbar">
+    <input id="input" placeholder="Type in Your Emoji here" onChange={emojiInput}></input>
+      <div id="meaning"> {meaning} </div>
     </div>
+      
+
+      <div id="list">
+        {emoji.map((item) => {
+          return <span onClick={() => clickHandler(item)}> {item} </span>;
+        })}
+      </div>
+    </section>
+
+      
+  </div>
   );
 }
-
-export default App;
